a26076a620054965241a711e6c63cabca903c464
bredele <olivier.wietrich@gmail.com>
2020-03-04 15:14:06 -0700

    Add code documentation and example

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 
+/**
+ * Execute promise callbacks one after an other
+ * and pass values.
+ *
+ * Examples:
+ *
+ *  chain(
+ *    value => Promise.resolve('hello'),
+ *    value => Promise.resolve(value + ' world!')
+ *  ).then(console.log)
+ *  // => hello world!
+ *
+ * @return {Promise}
+ * @public
+ */
 
 module.exports = (...args) => {
   return args.reduce((promise, next) => {
