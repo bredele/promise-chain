b8d964a268f61c5b84cff6f7127d220d9224b248
bredele <olivier.wietrich@gmail.com>
2020-03-09 09:44:15 -0600

    Cancel promise chain when one of the promises is rejected

--- test/promise-chain.js
+++ test/promise-chain.js
@@ -19,3 +19,12 @@ test('should execute multiple promise callbacks one after an other and pass valu
     value => Promise.resolve(value + ' world')
   ).then(data => assert.equal(data, 'hello world'))
 })
+
+test('should cancel promise chain', assert => {
+  assert.plan(1)
+  chain(
+    () => Promise.resolve('#1'),
+    () => Promise.reject('#2'),
+    () => Promise.resolve('#3').then(val => assert.equal(val, '#3'))
+  ).then(null, () => assert.ok('rejected at #2'))
+})
